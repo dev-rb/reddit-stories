3a26119b4b394fe52185ee1b0c253ca5579c7d05
dev-rb <43100342+dev-rb@users.noreply.github.com>
2021-09-18 21:18:43 -0400

    Finished sliding of download and read later features for touch devices. Need to add option for non-touch devices; possibly make it clickable on the side of the post.

--- components/Post/index.tsx
+++ components/Post/index.tsx
@@ -4,116 +4,52 @@ import styles from './post.module.css';
 import { CommentDetails, IPost } from '../../interfaces/reddit';
 import { MdFileDownload } from 'react-icons/md';
 import { BsClockHistory } from 'react-icons/bs';
+import { useSwipeControls } from '../../hooks/useSwipeControls';
 
 const Post = ({ title, id, score, author, permalink, stories, created }: IPost) => {
     // href={`https://www.reddit.com${permalink}`}
 
-    const [dragTimer, setDragTimer] = React.useState(0);
-    const [isDragging, setIsDragging] = React.useState(false);
-    const [mousePos, setMousePos] = React.useState(0);
-    const timerRef = React.useRef<NodeJS.Timer>();
-
     const postRef = React.useRef<HTMLDivElement>(null);
-    const swipeOptionsRef = React.useRef<HTMLDivElement>(null);
 
-    const onDragPost = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.preventDefault();
-        e.stopPropagation();
-        timerRef.current = setInterval(() => {
-            setDragTimer((prev) => prev + 1);
-        }, 50);
-    }
+    const [requests, setRequests] = React.useState({ download: false, readLater: false, pending: false });
 
-    const stopDragging = (e: React.MouseEvent<HTMLDivElement>) => {
-        e.stopPropagation();
-        e.preventDefault();
-        if (isDragging) {
-            resetDrag();
+    const updateForRequest = (typeOfRequest: string, setTo: boolean = false) => {
+        if (typeOfRequest === 'download') {
+            setRequests((prev) => ({ ...prev, download: true, pending: false }));
+        } else if (typeOfRequest === 'readLater') {
+            setRequests((prev) => ({ ...prev, readLater: true, pending: false }));
+        } else if (typeOfRequest === 'pending') {
+            setRequests((prev) => ({ ...prev, pending: true }));
         }
     }
 
-    const onDragging = (e: React.MouseEvent<HTMLDivElement>) => {
-        setMousePos(Math.min(Math.max(-100, e.clientX - 960), 100));
-    }
-
-    const handleSwipeOptions = (setTo: boolean, childIndex?: number, changeBoth: boolean = false) => {
-        let swipeOptions = swipeOptionsRef.current;
-
-        if (swipeOptions) {
-            if (!changeBoth) {
-                if (childIndex !== undefined) {
-                    swipeOptions.children[childIndex].toggleAttribute('visible', setTo);
-                }
-            } else {
-                swipeOptions.children[0].toggleAttribute('visible', setTo);
-                swipeOptions.children[1].toggleAttribute('visible', setTo);
-            }
-        }
-    }
-
-    const resetDrag = () => {
-        let post = postRef.current;
-        let timer = timerRef.current;
-
-        if (timer) {
-            clearInterval(timer);
-        }
-        setIsDragging(false);
-        handleSwipeOptions(false, undefined, true);
-
-        if (post) {
-            post.style.transform = 'none';
-            post.style.boxShadow = 'none';
-            setTimeout(() => { setDragTimer(0); }, 500)
-        }
-
-    }
-
-    React.useEffect(() => {
-        let timer = timerRef.current;
-        let post = postRef.current;
-        if (dragTimer == 2 && timer) {
-            clearInterval(timer);
-            setIsDragging(true);
-            if (post) {
-                post.style.boxShadow = '0px 10px 6px black';
-            }
-        }
-    }, [dragTimer])
-
-    React.useEffect(() => {
-        let post = postRef.current;
-
-        if (isDragging) {
-            if (post) {
-                if (mousePos <= -60) {
-                    handleSwipeOptions(true, 0);
-                } else if (mousePos >= 60) {
-                    handleSwipeOptions(true, 1);
-                } else {
-                    handleSwipeOptions(false, undefined, true);
-                }
-                post.style.transform = `translateX(${mousePos}px)`;
-            }
-        }
-    }, [mousePos])
-
-    React.useEffect(() => {
-        if (!isDragging) {
-            resetDrag();
-        }
-    }, [isDragging])
+    const { onDragPost, onDragging, onDragStop, downloadRequest, readLaterRequest } = useSwipeControls(postRef, updateForRequest);
 
     return (
         <Link scroll={false} href={`/posts/${id}`}>
             <div className={styles.postParent}>
-                <div ref={swipeOptionsRef} className={styles.swipeOptions}>
-                    <div className={styles.swipeDownload}> <MdFileDownload size={30} color="white" /> </div>
-                    <div className={styles.swipeReadLater}> <BsClockHistory size={30} color="white" /> </div>
-                </div>
-                <div ref={postRef} onClick={(e) => isDragging || dragTimer > 0 ? e.stopPropagation() : ''} onMouseDown={onDragPost} onMouseUp={stopDragging} onMouseLeave={stopDragging} onMouseMove={onDragging} className={styles.postContainer}>
+                {requests.pending &&
+                    <div className={styles.pendingRequest}>
+                        <h1> Loading... </h1>
+                    </div>
+                }
 
-                    <p> u/{author} </p>
+                {requests.pending != true ?
+                    <div className={styles.typeOfRequestBeingMade}>
+                        {downloadRequest ? <MdFileDownload size={40} color={'white'} /> : readLaterRequest ? <BsClockHistory size={40} color={'white'} /> : ''}
+                    </div>
+                    : ''
+                }
+
+                <div ref={postRef} onTouchStart={onDragPost} onTouchEnd={onDragStop} onTouchCancel={onDragStop} onTouchMove={onDragging} className={styles.postContainer}>
+
+                    <div className={styles.postHeaderInformation}>
+                        <p> u/{author} </p>
+                        <div className={styles.informationBadges}>
+                            <MdFileDownload size={16} color={requests.download ? '#3079F8' : '#313131'} />
+                            <BsClockHistory size={16} color={requests.readLater ? '#3079F8' : '#313131'} />
+                        </div>
+                    </div>
                     <div className={styles.title}>
                         <p> {title.replace('[WP]', '').trim()} </p>
                     </div>
