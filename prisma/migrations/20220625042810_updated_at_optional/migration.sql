7b9b3339d92f70b267f1c679f861cce164784f0d
dev-rb <43100342+dev-rb@users.noreply.github.com>
2023-03-02 18:44:16 -0500

    Downgrade prisma and delete migrations

--- prisma/migrations/20220625042810_updated_at_optional/migration.sql
+++
@@ -1,8 +0,0 @@
--- AlterTable
-ALTER TABLE `Post` MODIFY `updatedAt` DATETIME(3) NULL;
-
--- AlterTable
-ALTER TABLE `Reply` MODIFY `updatedAt` DATETIME(3) NULL;
-
--- AlterTable
-ALTER TABLE `Story` MODIFY `updatedAt` DATETIME(3) NULL;
