6180452f64270692da97102a81ae85246606d9ec
dev-rb <rbatra486@gmail.com>
2024-02-08 10:53:53 -0500

    Fresh start with solid-start.

--- src/types/sorts.ts
+++
@@ -1,4 +0,0 @@
-export type TopTimeSort = 'Today' | 'This Week' | 'This Month' | 'This Year' | 'All Time';
-export type TopSorts = 'day' | 'week' | 'month' | 'year' | 'all';
-export type SortType = 'Popular' | 'Top' | 'New';
-export type RedditSortTypeConversion = 'hot' | 'top' | 'new';
--- src/utils/trpc.ts
+++
@@ -1,4 +0,0 @@
-import { createReactQueryHooks } from '@trpc/react';
-import { AppRouter } from '../server/routers';
-
-export const trpc = createReactQueryHooks<AppRouter>();
