44e1bc2c7c9988f9fd061ae4ae0077f1d748b998
dev-rb <rbatra486@gmail.com>
2024-03-05 12:52:36 -0500

    Bump SolidStart and vinxi. Update to new config.

--- app.config.ts
+++ app.config.ts
@@ -2,14 +2,14 @@ import { defineConfig } from '@solidjs/start/config';
 import uno from 'unocss/vite';
 
 export default defineConfig({
-  plugins: [
-    // @ts-ignore
-    uno(),
-  ],
-  start: {
-    ssr: false,
-    server: {
-      preset: 'vercel',
-    },
+  vite: {
+    plugins: [
+      // @ts-ignore
+      uno(),
+    ],
+  },
+  ssr: false,
+  server: {
+    preset: 'vercel',
   },
 });
