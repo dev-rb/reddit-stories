ae53acdd7aa439cb21e202d0dbce6c128a92db56
dev-rb <rbatra486@gmail.com>
2024-03-05 13:52:47 -0500

    Update icons and manifest. Update meta and title.

--- src/entry-server.tsx
+++ src/entry-server.tsx
@@ -6,8 +6,18 @@ export default createHandler(() => (
       <html lang="en">
         <head>
           <meta charset="utf-8" />
-          <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <link rel="icon" href="/favicon.ico" />
+          <meta
+            name="viewport"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
+          />
+          <meta name="description" content="Site and PWA for r/writingprompts" />
+          <meta name="keywords" content="Reddit WritingPrompts Tavern Tales Stories Short" />
+
+          <title>Tavern Tales</title>
+
+          <link href="favicon.ico" rel="icon" type="image/png" sizes="32x32" />
+          <link rel="apple-touch-icon" href="/apple-touch-icon-180x180.png"></link>
+
           <link rel="manifest" href="/manifest.json" />
           {assets}
         </head>
